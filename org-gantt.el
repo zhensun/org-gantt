5cd4d767827f8f78618a25245916b762415d7282
stardiviner <numbchild@gmail.com>
2018-11-16 09:51:43 +0800

    Add a command to auto insert org-gantt dynamic block

--- org-gantt.el
+++ org-gantt.el
@@ -1478,6 +1478,25 @@ PARAMS determine several options of the gantt chart."
             (insert body))
           )))))
 
+(defun org-insert-dblock:org-gantt ()
+  "Insert org-gantt dynamic block."
+  (interactive)
+  (org-create-dblock
+   (list :name "org-gantt"
+         :file "data/images/project-gantt-chart.png"
+         :imagemagick t
+         :tikz-options "scale=1.5, every node/.style={scale=1.5}"
+         :weekend-style "{draw=blue!10, line width=1pt}"
+         :workday-style "{draw=blue!5, line width=.75pt}"
+         :show-progress 'if-value
+         :progress-source 'cookie-clocksum
+         :no-date-headlines 'inactive
+         :parameters "y unit title=.7cm, y unit chart=.9cm"
+         :tags-group-style '(("test"."group label font=\\color{blue}")
+                             ("toast"."group label font=\\color{green}"))
+         :tags-bar-style '(("test"."bar label font=\\color{blue}")
+                           ("toast"."bar label font=\\color{green}")))))
+
 (provide 'org-gantt)
 
 ;;; org-gantt.el ends here
